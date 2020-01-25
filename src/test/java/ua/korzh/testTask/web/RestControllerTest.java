7f2d91803cbf4dc6741eba08c4243c1315f597f1
Artem Korzh <korzhartem29@gmail.com>
2020-01-25 22:29:48 +0200

    Correct RestControllerTest.java one more time

--- src/test/java/ua/korzh/testTask/web/RestControllerTest.java
+++ src/test/java/ua/korzh/testTask/web/RestControllerTest.java
@@ -2,11 +2,14 @@ package ua.korzh.testTask.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ua.korzh.testTask.exception.EmailExistsException;
 import ua.korzh.testTask.model.Account;
@@ -20,8 +23,8 @@ import java.util.stream.Stream;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestController.class)
 public class RestControllerTest {
     @Autowired
     private MockMvc mockMvc;
