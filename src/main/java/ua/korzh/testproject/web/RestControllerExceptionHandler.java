1ba7dbc1febe6e999ec039968e0e10cf34287dd8
Artem Korzh <korzhartem29@gmail.com>
2020-04-06 13:43:03 +0300

    #15 Correct exception handler

--- src/main/java/ua/korzh/testproject/web/RestControllerExceptionHandler.java
+++ src/main/java/ua/korzh/testproject/web/RestControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package ua.korzh.testproject.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,18 +10,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ua.korzh.testproject.exception.*;
+import ua.korzh.testproject.service.client.ClientServiceImpl;
 
 @ControllerAdvice
 public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientServiceImpl.class);
 
     @ExceptionHandler({AccountNotExistException.class, NegativeAccountIdException.class, NegativeSumException.class, NotEnoughMoneyException.class})
     public ResponseEntity<Object> handleAccountNotExistOrNegativeAccountIdOrNegativeSumOrNotEnoughMoney(RuntimeException re, WebRequest request) {
+        LOGGER.error(re.getMessage(), re);
         return handleExceptionInternal(re, re.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EmailExistsException.class)
     public ResponseEntity<Object> handleEmailExists(RuntimeException re, WebRequest request) {
+        LOGGER.error(re.getMessage(), re);
         return handleExceptionInternal(re, re.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleExceptions(Exception e, WebRequest request) {
+        LOGGER.error(e.getMessage(), e);
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
