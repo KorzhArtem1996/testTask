a05b800e3de8d28ef106ce058146d5a9d978a1aa
Artem Korzh <korzhartem29@gmail.com>
2020-04-09 15:22:17 +0300

    #17 Replace CopyOnWriteArraySet to ConcurrentHashSet

--- src/main/java/ua/korzh/testproject/service/client/ClientServiceImpl.java
+++ src/main/java/ua/korzh/testproject/service/client/ClientServiceImpl.java
@@ -2,6 +2,7 @@ package ua.korzh.testproject.service.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -13,14 +14,17 @@ import ua.korzh.testproject.model.Client;
 import ua.korzh.testproject.repository.ClientRepository;
 import ua.korzh.testproject.service.account.AccountService;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @Service
-public class ClientServiceImpl implements ClientService {
+public class ClientServiceImpl implements ClientService, InitializingBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientServiceImpl.class);
-    private static final Set<String> EMAILS = new CopyOnWriteArraySet();
+    private static final ConcurrentHashMap<String, Integer> CONCURRENT_HASH_MAP = new ConcurrentHashMap<>();
+    private static final Set<String> EMAILS = CONCURRENT_HASH_MAP.newKeySet();
     private static final String REGISTER_START_MESSAGE = "register(email, password) started, email={}";
     private static final String REGISTER_SUCCESS_MESSAGE = "register(email, password) succeeded, email={}";
     private static final String DEPOSIT_START_MESSAGE = "deposit(money, accountId) started, money={}, accountId={}";
@@ -92,5 +96,13 @@ public class ClientServiceImpl implements ClientService {
         List<Transaction> res = accountService.history(accountId);
         LOGGER.info(SHOW_TRANSACTION_SUCCESS_MESSAGE, accountId);
         return res;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<Client> clients = clientRepository.findAll();
+        clients.forEach(c -> {
+                    EMAILS.add(c.getEmail());
+                });
     }
 }
