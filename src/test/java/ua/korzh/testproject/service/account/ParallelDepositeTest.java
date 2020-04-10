b5ae84f304ffcb50f438f6aa242e9ed2e8996257
Artem Korzh <korzhartem29@gmail.com>
2020-04-10 17:02:45 +0300

    #14 Correct ParallelDepositTest, finished

--- src/test/java/ua/korzh/testproject/service/account/ParallelDepositeTest.java
+++ src/test/java/ua/korzh/testproject/service/account/ParallelDepositeTest.java
@@ -8,19 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.transaction.annotation.Transactional;
-import ua.korzh.testproject.repository.AcountRepository;
 import ua.korzh.testproject.model.Client;
-import ua.korzh.testproject.repository.ClientRepository;
-import ua.korzh.testproject.service.account.AccountServiceImpl;
 import ua.korzh.testproject.service.client.ClientService;
 
-import javax.persistence.OptimisticLockException;
 
 @SpringBootTest
 public class ParallelDepositeTest {
 
-    @Autowired
-    private ClientRepository clientRepository;
     @SpyBean
     private AccountServiceImpl accountService;
     @Autowired
@@ -29,14 +23,15 @@ public class ParallelDepositeTest {
     @Test
     @Transactional
     public void parallelDepositTest() {
+        Client client = clientService.register("parallel", "deposit");
         long money = 100L;
         doAnswer(invocationOnMock -> {
-            accountService.deposit(150L, any());
+            accountService.deposit(150L, client.getAccountsId().get(0));
             return invocationOnMock.callRealMethod();
         }).when(accountService).createTransaction(any(), any(), eq(money));
 
-        Exception exception = assertThrows(OptimisticLockException.class, () ->{
-            accountService.deposit(money, 1);
+        Exception exception = assertThrows(ObjectOptimisticLockingFailureException.class, () ->{
+            accountService.deposit(money, client.getAccountsId().get(0));
         });
     }
 }
